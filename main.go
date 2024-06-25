65dfbc50aeedc1837a4a8aa2963c5851449d3ed1
Yun Ju Choi <yunju.dev@gmail.com>
2024-06-25 21:49:10 +0900

    1.8

--- main.go
+++ main.go
@@ -4,24 +4,16 @@ import (
 	"fmt"
 )
 
-func canIDrink(age int) bool {
-	// switch age {
-	// case 10:
-	// 	return false
-	// case 18:
-	// 	return true
-	// }
-	// return false
-
-	switch koreanAge := age + 2; koreanAge {
-	case 10:
-		return false
-	case 18:
-		return true
-	}
-	return false
-}
-
 func main() {
-	fmt.Println(canIDrink(16))
+	a := 2
+	// b := a
+	// a = 10
+
+	b := &a
+	*b = 20 // 주소값을 바라보기 때문에 해당 값이 바뀌면 a도 바뀌게 됨.
+
+	// fmt.Println(a, b)
+	fmt.Println(&a, &b) // 메모리 주소 확인
+	fmt.Println(*b)
+	fmt.Println(a)
 }
