e46b43af153d938a0d59c6e5f9f5a90d0651309a
Yun Ju Choi <yunju.dev@gmail.com>
2024-06-25 22:07:18 +0900

    1.11

--- main.go
+++ main.go
@@ -4,15 +4,14 @@ import (
 	"fmt"
 )
 
+type person struct {
+	name    string
+	age     int
+	favFood []string
+}
+
 func main() {
-	nico := map[string]string{"name": "nico", "age": "12"}
-	fmt.Println(nico)
-
-	for key, value := range nico {
-		fmt.Println(key, value)
-	}
-
-	for _, value := range nico {
-		fmt.Println(value)
-	}
+	favFood := []string{"kimchi", "ramen"}
+	nico := person{name: "nico", age: 18, favFood: favFood}
+	fmt.Println(nico.name)
 }
